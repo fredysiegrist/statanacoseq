7bca391614db72a1cbed477e7c296526a499167a
Fredy Siegrist <fredy.siegrist@students.unibe.ch>
2016-07-21 14:16:23 +0200

    void dummy of unknown function

---
+++ R/Entries.R
@@ -0,0 +1,25 @@
+#' @title Entries
+#'
+#' @description
+#' \code{Entries} DNA entries in the working data base
+#'
+#' @details
+#' Should work the same as Entries() in Darwin on DB
+#'
+#' @return VOID
+#'
+#' @author Roth, A.; Friberg, M.; Siegrist, F. and Cannarozzi, G. M. \email{gina@@cannarozzi.com}
+#' @seealso \code{\link{seqinr}} \code{\link{statanacoseq}} \code{\link{CodonProbabilities}}
+#' @keywords CodonBias
+#' @examples
+#' Entries()
+#'
+#' @import seqinr
+#'
+#' @export
+#' @section Original code in Darwin:
+#' \subsection{Compute CAI, the Codon Adaptation Index (Sharp and Li 1987)}{\preformatted{
+#' end: } }
+Entries <- function() {
+  # NEEDS TO BE FOUND IN DARWIN
+}
