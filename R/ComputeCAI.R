ce349810068bd772ce75c2beaa288bea9a26a093
Fredy Siegrist <fredy.siegrist@students.unibe.ch>
2016-07-26 17:48:21 +0200

    calculates index not vector

--- R/ComputeCAI.R
+++ R/ComputeCAI.R
@@ -7,14 +7,16 @@
 #' Should compute the same ComputeCAI or ComputeCAIVector as in Darwin and the cai of seqinr package.
 #'
 #' @param cds Coding sequence in reading frame
+#' @param RA Relative Adaptiveness table to use
+#' @param UseCodonProb Wheter to use Codon Probabilities (default = FALSE)
 #' @return Numerical with CAI
 #'
 #' @author Roth, A.; Friberg, M.; Siegrist, F. and Cannarozzi, G. M. \email{gina@@cannarozzi.com}
 #' @seealso \code{\link{seqinr}} \code{\link[seqinr]{cai}} \code{\link{statanacoseq}} \code{\link{checkCDS}}
 #' @keywords CodonBias
 #' @examples
-#' ComputeCAI('ATGTGGTACTCCGACTACGGAGGATAA', RA=SetupRA("yeast"))
-#' ComputeCAI(c2s(mylist(whatout=1)[[1]]), RA=ComputeCarboneRA(DB=mylist(whatout=1)))
+#' ComputeCAI('ATGTGGTACTCCGACTACGGAGGATAA', RA=SetupRA("yeast"), UseCodonProb=TRUE)
+#' ComputeCAI(mylist(whatout=1)[[1]], RA=ComputeCarboneRA(DB=mylist(whatout=1)))
 #'
 #' @import seqinr
 #'
@@ -46,55 +48,27 @@
 #'    w := w + ln(codprob * RA[cint]) fi;
 #'  od;
 #'  exp(1/n * w)
-#' end:
-#'
-#' ComputeCAIVector := proc(e:Entry)
-#'  if not assigned(RA) then
-#'    error('Error in ComputeCAI: RA not assigned, use e.g. SetupRA(yeast);') fi;
-#'  dna := SearchTag('DNA', e);
-#'  wa := CreateArray(1..20);
-#'  na := CreateArray(1..20);
-#'  for j to length(dna) by 3 do
-#'    cint := CodonToCInt(dna[j..j+2]);
-#'    a := CIntToInt(cint);
-#'    if a <= 20 then
-#'      wa[a] := wa[a] + ln(RA[cint]);
-#'      na[a] := na[a]+1;
-#'    fi;
-#'  od;
-#'  res := CreateArray(1..21);
-#'  for i to 20 do
-#'    res[i] := If(na[i]=0, 'NA', exp(1/na[i] * wa[i])) od;
-#'  res[21] := exp(1/sum(na) * sum(wa));
-#'  res
 #' end: } }
-
-ComputeCAI <- function(cds, RA) {
-  if(!(checkCDS(cds))) {stop("non valid CDS)", call.=FALSE)}
+ComputeCAI <- function(cds, RA, UseCodonProb=FALSE) {
+  dna <- toupper(c2s(cds))
+  if(!(checkCDS(dna))) {stop("non valid CDS)", call.=FALSE)}
   else {
     if (missing(RA)) {
       stop('Error in ComputeCAI: RA not assigned, use e.g. RA=SetupRA("yeast")', call. = FALSE)
     }
-    a.uni <- unique(a(substr(names(aa_ac), 1, 3)[c(-59, -60, -64, -65)]))
-    dna <- toupper(cds)
-    if(!(checkCDS(dna))) stop("non valid CDS)", call.=FALSE)
-    wa <- rep(0, times=20)
-    na <- rep(0, times=20)
+    cai <- NA
+    w <- 0
+    n <- nchar(dna)/3;
     for (j in seq(from=1, to=nchar(dna), by=3)) {
       codon <- reversecomplement(substr(dna, j, j+2))
       cint <- which(substr(names(aa_ac), 5, 7) %in% codon)
-      if (cint == 65) break # to avoid XXX
-      a <- which(a.uni %in% a(substr(names(aa_ac), 1, 3)[cint]))
-      if (a <= 20) {
-        wa[a] <- wa[a] + log(RA[cint])
-        na[a] <- na[a]+1
+      if (UseCodonProb) {codprob <- CodonProb(cint)}
+      else {codprob <- 1}
+      if (!(cint %in% c(59,60,64,65)))  {  # don't consider stop codons
+        w <- w + log(codprob * RA[cint])
       }
     }
-    res <- rep(NA, times=21)
-    for (i in 1:20) {
-      if(!(na[i]==0)) res[i] <- exp(1/na[i] * wa[i])
-    }
-    res[21] <- exp(1/sum(na) * sum(wa))
-    return(res)
+    cai <- exp(1/n * w)
+    return(unname(cai))
   }
 }
